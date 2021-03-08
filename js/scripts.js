1d71757b8d336698034b892923613c0d920affea
veetance <divineokos@hotmail.com>
2021-03-08 02:45:21 -0700

    js object data structure quote template complete with button

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,8 @@ function Quote( quoteT, authorT, tagsT, colorT) {
 		var container = $("<div>")
 			this.tags.forEach(function(tag){
 				container.addClass(tag);
-				$(".buttons").prepend("<button>" + tag + "</button>")
-
 			})
+
 		container.css("background", this.color)
 
 		var quoteString = "";
@@ -26,18 +25,49 @@ function Quote( quoteT, authorT, tagsT, colorT) {
 		quoteString += "<cite>" + this.author + "</cite>";
 
 		container.html(quoteString)
-		$(".quotes"). prepend(container)
+		$(".quotes").prepend(container)
 	}
 }
 
 var quotes = [
  new Quote('"work smarter not harder. Don\'t waste your time."',"- Veetance",["Advice", "Truth", "mottos"],"#4287f5"),
- new Quote('"Grip fast knowledge"', '- Loral in\'t school', ["mottos", "primary schools"],"#eb4034")
+ new Quote('"Grip fast knowledge"', "- Loral in\'t school", ["mottos", "primary_schools"],"#eb4034")
 ]
+
+
+//global tag list
+var tagList = []
 
 quotes.forEach( function (quote){
 	quote.display();
+	quote.tags.forEach(function(tag){
+		//check to see if tag has been added to taglist 
+		if(!tagList.includes(tag)) {
+		//if it isnt added, add it
+		tagList.push(tag);
+		// and also make a button for it.
+		$(".buttons").prepend("<button class='filter' id='" + tag + "'>" + tag + "</button>");
+		}
+
+	})
+
 });
+
+console.log(tagList)
+
+$(".filter").on("click", function() {
+	var tag = $(this).attr("id");
+	console.log(tag)
+	
+	$("div > div").not("." + tag).hide();
+	$("." + tag).fadeIn();
+
+	$(".filter").removeClass("active")
+	$(this).addClass("active")
+
+	
+
+})
 
 
 
