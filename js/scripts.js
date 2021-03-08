5c4a87279aa123b7a25fd8e583c2bf4393ddbc24
veetance <divineokos@hotmail.com>
2021-03-07 21:08:55 -0700

    Quote blocks added with js

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,65 @@ var quotes = [
 '"Grip fast knowledge" - Loral in\'t school'
 ];
 
-console.log(quotes);
+var quote1 = {
+	quote: '"work smarter not harder. Don\'t waste your time."',
+	author: "Veetance",
+	tags: ["Advice", "Truth"],
+	color: "#4287f5", //blue
+	
+	display: function() {
+		var quoteString = "";
+		quoteString += "<div style='background:" + this.color + "'>";
+		quoteString += "<p>" + this.quote + "</p>";
+		quoteString += "<cite>" + this.author + "</cite>";
+		quoteString += "</div>"
+		$("body"). prepend(quoteString)
+	}
+}
 
-var quoteString = "";
-quotes.forEach(function(quote){
-
-	quoteString += "<p>" + quote + "</p>"
+quote1.display();
 
 
-	console.log(quoteString);
 
-})
+var quote2 = {
+	quote: '"Grip fast knowledge"',
+	author: "Loral Int'l school",
+	tags: ["Advice", "Truth"],
+	color: "#fcba03", //blue
+	
+	
 
-$("body").html(quoteString)
+	display: function() {
+		var quoteString = "";
+		quoteString += "<div style='background:" + this.color + "'>";
+		quoteString += "<p>" + this.quote + "</p>";
+		quoteString += "<cite>" + this.author + "</cite>";
+		quoteString += "</div>"
+		$("body"). prepend(quoteString)
+	}
+}
+
+quote2.display();
+
+
+
+
+
+
+
+
+
+console.log(quote1.quote);
+
+// var quoteString = "";
+// quotes.forEach(function(quote){
+
+// 	quoteString += "<p>" + quote + "</p>"
+
+
+// 	console.log(quoteString);
+
+// })
+
+// $("body").html(quoteString)
 
